e8325fe7162186ed2ddc3a78e285b826f6290597
godwin-nj <amadigodwin7@gmail.com>
2022-02-25 16:05:34 +0100

    prop-types

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ name, image, price }) => {
+  // console.log(image, name, price);
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <h2>{name}</h2>
+      <img src={url || defaultImage} alt={name || "defaultImage"} />
+      <h2>${price || 3.99}</h2>
+    </article>
+  );
 };
+
+// Product.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   price: PropTypes.number.isRequired,
+//   image: PropTypes.object.isRequired,
+// };
+
+// Product.defaultProps = {
+//   name: "default image",
+//   price: 3.99,
+//   image: defaultImage,
+// };
 
 export default Product;
